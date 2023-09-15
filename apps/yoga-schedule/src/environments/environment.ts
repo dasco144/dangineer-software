da55c6c30a2f5b056aad06c707feb7de22ce6fc8
Daniel Santiago <danielsantiago@outlook.com>
2023-09-15 21:32:21 +0200

    add firebase

---
+++ apps/yoga-schedule/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'yoga-schedule-fd388',
+    appId: '1:983355420051:web:86bbad769098022ecf4c66',
+    storageBucket: 'yoga-schedule-fd388.appspot.com',
+    apiKey: 'AIzaSyCzZjf2LWiaEkfCfTnuE_uqbiKcxEQCONk',
+    authDomain: 'yoga-schedule-fd388.firebaseapp.com',
+    messagingSenderId: '983355420051',
+    measurementId: 'G-Y7CNZVHSCC',
+  },
+};
--- apps/yoga-schedule/src/main.ts
+++ apps/yoga-schedule/src/main.ts
@@ -4,11 +4,24 @@ import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { importProvidersFrom } from '@angular/core';
 import appRoutes from './app/app.routes';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
+
+// Add angular fire config here
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot()),
     provideRouter(appRoutes),
+    importProvidersFrom(
+      IonicModule.forRoot(),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage())
+    ),
   ],
 });
