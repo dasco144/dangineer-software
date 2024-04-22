4bad1e1d8b8d7da6f363befe3642ddeb57e8fa26
Daniel Santiago <dasco144@hotmail.com>
2024-04-22 20:56:31 +0200

    fix standalone migration by adding missing bits

--- apps/yoga-schedule/src/main.ts
+++ apps/yoga-schedule/src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { importProvidersFrom } from '@angular/core';
 import appRoutes from './app/app.routes';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
@@ -9,15 +8,16 @@ import { provideStorage, getStorage } from '@angular/fire/storage';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 // Add angular fire config here
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
     provideRouter(appRoutes),
     importProvidersFrom(
-      IonicModule.forRoot(),
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideAuth(() => getAuth()),
       provideFirestore(() => getFirestore()),
