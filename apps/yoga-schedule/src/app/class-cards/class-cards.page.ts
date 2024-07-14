0085f07ce852f5e4275dc321a4841930c0193dd9
Daniel Santiago <dasco144@hotmail.com>
2024-07-14 13:22:36 +0200

    chore: update component selectors

--- apps/yoga-schedule/src/app/class-cards/class-cards.page.ts
+++ apps/yoga-schedule/src/app/class-cards/class-cards.page.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'dangineer-software-class-cards',
+  selector: 'dgs-class-cards',
   templateUrl: './class-cards.page.html',
   styleUrls: ['./class-cards.page.scss'],
   standalone: true,
   imports: [],
 })
-export default class ClassCardPage {
-  constructor() {}
-}
+export default class ClassCardPage {}
